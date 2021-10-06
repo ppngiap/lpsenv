a35160dabc3313823e6c31ac7903fb5a877ce767
Tom Phan <ppngiap@gmail.com>
2021-10-06 15:30:09 -0700

    Update print output.

--- py/lpsenv.py
+++ py/lpsenv.py
@@ -3,12 +3,19 @@ def exports(filename):
     for line in lines:
         if line.strip().startswith("export "):
             yield line
-    return None
+
+def removeprefix(str, prefix):
+    if str.find(prefix) == 0:
+        return str[len(prefix):]
+    return str # no change
 
 
 libenv = {}
 for line in exports("swdepot.bashrc"):
-    [k, v] = line.strip().removeprefix("export ").split("=")
+    # [k, v] = line.strip().removeprefix("export ").split("=")
+    [k, v] = removeprefix(line.strip(), "export ").split("=")
     libenv[k] = v
 
+for (key, val) in libenv.items():
+    print(key + ":" + val)
 print(libenv)
