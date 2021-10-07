28c87b75b13bcef148a3a0dc77a2183b68f1fa96
tom phan <ppngiap@gmail.com>
2021-10-06 19:28:59 -0700

    Add bdevenv aliases

--- py/lpsenv.py
+++ py/lpsenv.py
@@ -1,13 +1,16 @@
+import os
+
 def exports(filename):
     lines = open(filename).readlines()
     for line in lines:
         if line.strip().startswith("export "):
             yield line
 
+
 def removeprefix(str, prefix):
     if str.find(prefix) == 0:
         return str[len(prefix):]
-    return str # no change
+    return str  # no change
 
 
 libenv = {}
@@ -16,6 +19,13 @@ for line in exports("swdepot.bashrc"):
     [k, v] = removeprefix(line.strip(), "export ").split("=")
     libenv[k] = v
 
+try:
+    libdir = os.environ['LPS_BASE_LIBDIR']
+except KeyError:
+    print("LPS_BASE_LIBDIR has not been exported")
+    exit(-1)
+
 for (key, val) in libenv.items():
-    print(key + ":" + val)
-print(libenv)
+    print(key + "=" + libdir)
+
+#print(libenv)
